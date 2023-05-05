7868bc764845718ba0c27cd79ba816fac03b42af
xlearns <2447684095@qq.com>
2023-05-05 10:32:13 +0800

    chore: add utility description for ts challenge

--- Ts体操/example/Utility.ts
+++ Ts体操/example/Utility.ts
@@ -17,28 +17,39 @@ declare function DParameters<T>(name: string, age: number, test: null): T;
 
 type CatName = "miffy" | "boris" | "mordred";
 
+// 根据union提取由type/interface声明的类型
 type TestPickType = Pick<ITodo, "title" | "age" | "test">;
 
+// 根据union排除由type/interface声明的类型
 type TestOmitType = Omit<ITodo, "title" | "age" | "test">;
 
+// 提取union中的类型
 type TestExtractType = Extract<UnionType, "a" | "b" | "z">;
 
+// 排除union中的类型
 type TestExcludeType = Exclude<UnionType, "a" | "b" | "z">;
 
+// 给所有union类型，设置对应的类型
 type TestRecordType = Record<UnionType, CatInfo>;
 
+// 提取函数的参数
 type TestParameters1Type = Parameters<(name: string, age: number) => void>;
 
 type TestParameters2Type = Parameters<typeof DParameters>;
 
+// 提取函数的return
 type TestReturnTypeType = ReturnType<(name: string, age: number) => void>;
 
 type TestReturnType2Type = ReturnType<typeof DParameters>;
 
+// 提取promise最终传递的值可以是union类型
 type TestAwaitedType = Awaited<boolean | Promise<Promise<number>>>;
 
+// 所有属性变成可选
 type TestPartialType = Partial<CatInfo>;
 
+// 所有属性变成必选
 type TestRequiredType = Required<TestPartialType>;
 
+// 所有属性变成只读
 type TestReadonlyType = Readonly<TestPartialType>;
